d12af70d51a5b3881794ea2c4d55e14568883094
Christoph Werner <christoph.werner@smartclip.tv>
2025-11-29 13:28:36 +0100

    chore: generate lcov coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     coverage: {
       provider: 'v8',
-      reporter: ['text'],
+      reporter: ['lcov', 'text'],
       include: ['src/**'],
       exclude: ['src/setup.ts', 'src/dts/**'],
     },
